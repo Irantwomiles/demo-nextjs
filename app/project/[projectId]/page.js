38d861237dfd3d2bbee65c437d0a8a4bf95d8e9b
Shayan Ghofrany <irantwomiles@gmail.com>
2023-05-07 13:26:58 -0400

    dynamic page

---
+++ app/project/[projectId]/page.js
@@ -0,0 +1,18 @@
+'use client';
+
+import {useEffect} from "react";
+import {useSearchParams, usePathname} from "next/navigation";
+
+export default function Project() {
+
+    const pathname = usePathname();
+    const query = pathname.split('/').pop();
+
+    console.log(query)
+
+    return (
+        <main>
+            <div>Shayan {query}</div>
+        </main>
+    )
+}
