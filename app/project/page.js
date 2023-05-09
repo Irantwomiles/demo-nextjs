736ea470a9e98c56ec7736901b53817af6cfd403
Shayan Ghofrany <irantwomiles@gmail.com>
2023-05-09 19:44:18 -0400

    removed dynamic route

---
+++ app/project/page.js
@@ -0,0 +1,14 @@
+'use client';
+
+import {useEffect} from "react";
+
+export default function Project() {
+
+    return (
+        <main>
+            <div>Page 1</div>
+        </main>
+    )
+}
+
+export const runtime = 'edge';
